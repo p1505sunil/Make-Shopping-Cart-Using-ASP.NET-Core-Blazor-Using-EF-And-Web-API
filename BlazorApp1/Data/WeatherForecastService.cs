b0a66e7e2966d6e378cce9937c06c4e1efcbcea5
venkatesh kalva <venkateshkalva@yahoo.in>
2019-12-07 14:57:33 -0600

    pass the shopping details to save the result in our ShoppingDetails table in our database.

--- BlazorApp1/Data/WeatherForecastService.cs
+++ BlazorApp1/Data/WeatherForecastService.cs
@@ -10,7 +10,7 @@ namespace BlazorApp1.Data
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
+            
         public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
             var rng = new Random();
--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -17,7 +17,7 @@ namespace BlazorApp1
         {
             CreateHostBuilder(args).Build().Run();
         }
-
+           
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
